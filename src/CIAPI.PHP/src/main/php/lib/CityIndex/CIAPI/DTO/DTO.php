a70e31be4d17f6121f05cf7d846d98053e7cc81d
Steffen Opel <sopel@opelbrothers.net>
2012-04-25 02:20:15 +0200

    Added missing app related attributes to ApiLogOnRequestDTO.
    [Issue(s) CIAPI-28]

--- src/CIAPI.PHP/src/main/php/lib/CityIndex/CIAPI/DTO/DTO.php
+++ src/CIAPI.PHP/src/main/php/lib/CityIndex/CIAPI/DTO/DTO.php
@@ -7,18 +7,32 @@ namespace CityIndex\CIAPI\DTO;
  * Request to create a session (log in).
  */
 class ApiLogOnRequestDTO {
+	const userNameMinLength = 6;
+	const userNameMaxLength = 20;
+	const passwordMinLength = 6;
+	const passwordMaxLength = 20;
+	
 	private $userName;
 	private $password;
+	private $appKey;
+	private $appVersion;
+	private $appComments;
 	
     /**
      * Initalize the class attribute info.
      *  
      * @param string $userName
      * @param string $password
+     * @param string $appKey
+     * @param string $appVersion
+     * @param string $appComments (optional)
      */
-    public function __construct($userName, $password) {
+    public function __construct($userName, $password, $appKey, $appVersion, $appComments = "") {
         $this->userName = $userName;
         $this->password = $password;
+        $this->appKey = $appKey;
+        $this->appVersion = $appVersion;
+        $this->appComments = $appComments;
     }
 
 	public function getUserName()
@@ -29,6 +43,21 @@ class ApiLogOnRequestDTO {
 	public function getPassword()
 	{
 		return $this->password;
+	}
+
+	public function getAppKey()
+	{
+		return $this->appKey;
+	}
+
+	public function getAppVersion()
+	{
+		return $this->appVersion;
+	}
+
+	public function getAppComments()
+	{
+		return $this->appComments;
 	}
 }
 
