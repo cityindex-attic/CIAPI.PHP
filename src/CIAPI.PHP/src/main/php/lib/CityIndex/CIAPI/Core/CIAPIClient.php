cityindex-attic/CIAPI.PHP

commit db0c631f11ef5bbbe0e3bc7e09e84404e80397da

Added initial composer definition.
[Issue(s) CIAPI-46]

src/CIAPI.PHP/src/main/php/lib/CityIndex/CIAPI/Core/CIAPIClient.php
@@ -2,8 +2,6 @@
 
 namespace CityIndex\CIAPI\Core;
 
-use CityIndex\CIAPI\DTO\ApiLogOnRequestDTO;
-
 require_once 'lib/CityIndex/CIAPI/Core/CIAPI.php';
 require_once 'lib/CityIndex/CIAPI/DTO/DTO.php';
 
@@ -16,19 +14,7 @@ use CityIndex\CIAPI\DTO\ApiLogOnResponseDTO;
 class CIAPIClient implements CIAPI {
 	private $userName;
 	private $session;
-	private $appKey;
-	private $appVersion;
-	private $appComments;
-
-	/**
-	 * Initalize the class attribute info.
-	 */
-	public function __construct($appKey, $appVersion, $appComments = "") {
-		$this->appKey = $appKey;
-		$this->appVersion = $appVersion;
-		$this->appComments = $appComments;
-	}
-
+	
 	/**
 	 * Log In
 	 *
@@ -38,9 +24,7 @@ class CIAPIClient implements CIAPI {
 	 */
 	public function logIn($userName, $password) {
 		$this->userName = $userName;
-		$request = new ApiLogOnRequestDTO($userName, $password,
-				$this->appKey, $this->appVersion, $this->appComments);
-
+		
 		return null;
 	}
 
@@ -51,7 +35,7 @@ class CIAPIClient implements CIAPI {
 	 */
 	public function logOut() {
 		$this->session = null;
-
+		
 		return true;
 	}
 
@@ -67,7 +51,7 @@ class CIAPIClient implements CIAPI {
 		// throw new RuntimeException("Not implemented!");
 		return null;
 	}
-
+	
 	/**
 	 * Get UserName
 	 *
@@ -84,35 +68,5 @@ class CIAPIClient implements CIAPI {
 	 */
 	public function getSession() {
 		return $this->session;
-	}
-
-	/**
-	 * Get App Key
-	 *
-	 * @return string $appKey
-	 */
-	public function getAppKey()
-	{
-		return $this->appKey;
-	}
-
-	/**
-	 * Get App Version
-	 *
-	 * @return string $appVersion
-	 */
-	public function getAppVersion()
-	{
-		return $this->appVersion;
-	}
-
-	/**
-	 * Get App Comments
-	 *
-	 * @return string $appComments
-	 */
-	public function getAppComments()
-	{
-		return $this->appComments;
 	}
 }

src/CIAPI.PHP/src/test/php/CityIndex/CIAPI/Test/CIAPITest.php
@@ -6,14 +6,12 @@ use CityIndex\CIAPI\Core\CIAPIClient;
 
 
 class CIAPITest extends PHPUnit_Framework_TestCase {
-	static private $userName = 'NOTAUSERNAME';
-	static private $password = 'NOTAPASSWORD';
-	static private $appKey = 'CIAPITest';
-	static private $appVersion = '1.0-SNAPSHOT';	// @todo: derive automatically.
 	protected $ctx;
+	static private $userName = "NOTAUSERNAME";
+	static private $password = "NOTAPASSWORD";
 	
 	protected function setUp() {
- 		$this->ctx = new CIAPIClient(self::$appKey, self::$appVersion);
+ 		$this->ctx = new CIAPIClient();
 	}
 
 	/**

src/CIAPI.PHP/.gitignore
@@ -1,0 +1,1 @@
+vendor

commit e1ff46d101b6f7feccb1fc96d3522d9aa9cecfe4

Partially reverted commit db0c631f, which accidentally reverted parts of commit a70e31be in turn.
[Issue(s) CIAPI-51]

src/CIAPI.PHP/src/main/php/lib/CityIndex/CIAPI/Core/CIAPIClient.php
@@ -2,6 +2,8 @@
 
 namespace CityIndex\CIAPI\Core;
 
+use CityIndex\CIAPI\DTO\ApiLogOnRequestDTO;
+
 require_once 'lib/CityIndex/CIAPI/Core/CIAPI.php';
 require_once 'lib/CityIndex/CIAPI/DTO/DTO.php';
 
@@ -14,7 +16,19 @@ use CityIndex\CIAPI\DTO\ApiLogOnResponseDTO;
 class CIAPIClient implements CIAPI {
 	private $userName;
 	private $session;
-	
+	private $appKey;
+	private $appVersion;
+	private $appComments;
+
+	/**
+	 * Initalize the class attribute info.
+	 */
+	public function __construct($appKey, $appVersion, $appComments = "") {
+		$this->appKey = $appKey;
+		$this->appVersion = $appVersion;
+		$this->appComments = $appComments;
+	}
+
 	/**
 	 * Log In
 	 *
@@ -24,7 +38,9 @@ class CIAPIClient implements CIAPI {
 	 */
 	public function logIn($userName, $password) {
 		$this->userName = $userName;
-		
+		$request = new ApiLogOnRequestDTO($userName, $password,
+				$this->appKey, $this->appVersion, $this->appComments);
+
 		return null;
 	}
 
@@ -35,7 +51,7 @@ class CIAPIClient implements CIAPI {
 	 */
 	public function logOut() {
 		$this->session = null;
-		
+
 		return true;
 	}
 
@@ -51,7 +67,7 @@ class CIAPIClient implements CIAPI {
 		// throw new RuntimeException("Not implemented!");
 		return null;
 	}
-	
+
 	/**
 	 * Get UserName
 	 *
@@ -68,5 +84,35 @@ class CIAPIClient implements CIAPI {
 	 */
 	public function getSession() {
 		return $this->session;
+	}
+
+	/**
+	 * Get App Key
+	 *
+	 * @return string $appKey
+	 */
+	public function getAppKey()
+	{
+		return $this->appKey;
+	}
+
+	/**
+	 * Get App Version
+	 *
+	 * @return string $appVersion
+	 */
+	public function getAppVersion()
+	{
+		return $this->appVersion;
+	}
+
+	/**
+	 * Get App Comments
+	 *
+	 * @return string $appComments
+	 */
+	public function getAppComments()
+	{
+		return $this->appComments;
 	}
 }

src/CIAPI.PHP/src/test/php/CityIndex/CIAPI/Test/CIAPITest.php
@@ -6,12 +6,14 @@ use CityIndex\CIAPI\Core\CIAPIClient;
 
 
 class CIAPITest extends PHPUnit_Framework_TestCase {
+	static private $userName = 'NOTAUSERNAME';
+	static private $password = 'NOTAPASSWORD';
+	static private $appKey = 'CIAPITest';
+	static private $appVersion = '1.0-SNAPSHOT';	// @todo: derive automatically.
 	protected $ctx;
-	static private $userName = "NOTAUSERNAME";
-	static private $password = "NOTAPASSWORD";
 	
 	protected function setUp() {
- 		$this->ctx = new CIAPIClient();
+ 		$this->ctx = new CIAPIClient(self::$appKey, self::$appVersion);
 	}
 
 	/**