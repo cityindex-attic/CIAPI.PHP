bb9b347b3c4f10ef84cbc1062dccadd4c3c7615e
Steffen Opel <sopel@opelbrothers.net>
2012-07-19 16:09:58 +0200

    Added missing DTOs for login/session test scenario.
    [Issue(s) CIAPI-28]

---
+++ src/CIAPI.PHP/src/main/php/lib/CityIndex/CIAPI/DTO/AccountInformationResponseDTO.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace CityIndex\CIAPI\DTO;
+
+/**
+ * Response from an account information query.
+ */
+class AccountInformationResponseDTO {
+	private $logonUserName;
+	private $clientAccountId;
+	private $clientAccountCurrency;
+	private $accountOperatorId;
+	private $tradingAccounts = array();
+	private $personalEmailAddress;
+	private $hasMultipleEmailAddresses;
+	
+	/**
+	 * Initalize the class attribute info.
+	 *  
+	 * @param string $logonUserName
+	 * @param integer $clientAccountId
+	 * @param string $clientAccountCurrency
+	 * @param integer $accountOperatorId
+	 * @param array $tradingAccounts
+	 * @param string $personalEmailAddress
+	 * @param bool $personalEmailAddress
+	 */
+	public function __construct($logonUserName, $clientAccountId, $clientAccountCurrency, $accountOperatorId, 
+			array $tradingAccounts, $personalEmailAddress, $hasMultipleEmailAddresses) {
+		$this->logonUserName = $logonUserName;
+		$this->clientAccountId = $clientAccountId;
+		$this->clientAccountCurrency = $clientAccountCurrency;
+		$this->accountOperatorId = $accountOperatorId;
+		$this->tradingAccounts = $tradingAccounts;
+		$this->personalEmailAddress = $personalEmailAddress;
+		$this->hasMultipleEmailAddresses = $hasMultipleEmailAddresses;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLogonUserName() {
+		return $this->logonUserName;
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getClientAccountId() {
+		return $this->clientAccountId;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getClientAccountCurrency() {
+		return $this->clientAccountCurrency;
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getAccountOperatorId() {
+		return $this->accountOperatorId;
+	}
+
+	/**
+	 * A list of trading accounts.
+	 *
+	 * @return null|array
+	 */
+ 	public function getTradingAccounts() {
+		return $this->tradingAccounts;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPersonalEmailAddress() {
+		return $this->personalEmailAddress;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getHasMultipleEmailAddresses() {
+		return $this->hasMultipleEmailAddresses;
+	}
+}
---
+++ src/CIAPI.PHP/src/main/php/lib/CityIndex/CIAPI/DTO/ApiTradingAccountDTO.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace CityIndex\CIAPI\DTO;
+
+/**
+ * Information about a Trading Account.
+ */
+class ApiTradingAccountDTO {
+	const tradingAccountStatusOpen = 'Open';
+	const tradingAccountStatusClosed = 'Closed';
+	const tradingAccountTypeSpread = 'Spread';
+	const tradingAccountTypeCFD = 'CFD';
+	
+	private $tradingAccountId;
+	private $tradingAccountCode;
+	private $tradingAccountStatus;
+	private $tradingAccountType;
+
+	/**
+	 * Initalize the class attribute info.
+	 *  
+	 * @param integer $tradingAccountId
+	 * @param string $tradingAccountCode
+	 * @param string $tradingAccountStatus
+	 * @param string $tradingAccountType
+	 */
+	public function __construct($tradingAccountId, $tradingAccountCode, $tradingAccountStatus, $tradingAccountType) {
+		$this->tradingAccountId = $tradingAccountId;
+		$this->tradingAccountCode = $tradingAccountCode;
+		$this->tradingAccountStatus = $tradingAccountStatus;
+		$this->tradingAccountType = $tradingAccountType;
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getTradingAccountId() {
+		return $this->tradingAccountId;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTradingAccountCode() {
+		return $this->tradingAccountCode;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTradingAccountStatus() {
+		return $this->tradingAccountStatus;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTradingAccountType() {
+		return $this->tradingAccountType;
+	}
+}
