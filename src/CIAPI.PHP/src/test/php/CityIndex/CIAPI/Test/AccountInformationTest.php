c3370cf865265de3e323d6dda6b7c665fc83b02c
Steffen Opel <sopel@opelbrothers.net>
2012-07-25 14:40:01 +0200

    Refactored test class hierarchy:
    - Extracted class CIAPITestCase.
    
    [Issue(s) CIAPI-28]

--- src/CIAPI.PHP/src/test/php/CityIndex/CIAPI/Test/AccountInformationTest.php
+++ src/CIAPI.PHP/src/test/php/CityIndex/CIAPI/Test/AccountInformationTest.php
@@ -3,25 +3,19 @@
 namespace CityIndex\CIAPI\Test;
 // @todo: this is rather kludgy ...
 require_once __DIR__ . '/../../../../../../vendor/autoload.php';
+require_once 'CIAPITestCase.php';
 
 use CityIndex\CIAPI\Core\CIAPIClient;
 use CityIndex\CIAPI\Core\SessionException;
 use CityIndex\CIAPI\DTO\AccountInformationResponseDTO;
 
-class AccountInformationTest extends \PHPUnit_Framework_TestCase {
-	static private $endpoint = 'https://example.com/';
-	static private $userName = 'NOTAUSERNAME';
-	static private $password = 'NOTAPASSWORD';
-	static private $appKey = 'CIAPI.PHP';
-	static private $appVersion = '1.0-SNAPSHOT'; // @todo: derive automatically.
-	protected static $ctx;
+class AccountInformationTest extends CIAPITestCase {
 
 	public static function setUpBeforeClass() {
-		self::$ctx = new CIAPIClient(self::$endpoint, self::$appKey, self::$appVersion);
 		self::$ctx->logIn(self::$userName, self::$password);
 	}
 
-	protected function tearDown() {
+	public static function tearDownAfterClass() {
 		self::$ctx->logOut();
 	}
 
--- src/CIAPI.PHP/src/test/php/CityIndex/CIAPI/Test/AuthenticationTest.php
+++ src/CIAPI.PHP/src/test/php/CityIndex/CIAPI/Test/AuthenticationTest.php
@@ -3,23 +3,13 @@
 namespace CityIndex\CIAPI\Test;
 // @todo: this is rather kludgy ...
 require_once __DIR__ . '/../../../../../../vendor/autoload.php';
+require_once 'CIAPITestCase.php';
 
 use CityIndex\CIAPI\Core\CIAPIClient;
 use CityIndex\CIAPI\Core\SessionException;
 use CityIndex\CIAPI\DTO\AccountInformationResponseDTO;
 
-class AuthenticationTest extends \PHPUnit_Framework_TestCase {
-	static private $endpoint = 'https://example.com/';
-	static private $userName = 'NOTAUSERNAME';
-	static private $password = 'NOTAPASSWORD';
-	static private $appKey = 'CIAPI.PHP';
-	static private $appVersion = '1.0-SNAPSHOT'; // @todo: derive automatically.
-	protected static $ctx;
-
-	public static function setUpBeforeClass() {
-		self::$ctx = new CIAPIClient(self::$endpoint, self::$appKey, self::$appVersion);
-	}
-
+class AuthenticationTest extends CIAPITestCase {
 	/**
 	 * @test
 	 * @expectedException CityIndex\CIAPI\Core\SessionException
